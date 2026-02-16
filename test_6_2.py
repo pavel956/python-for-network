bc3e1c32030e2ebaa98e0058460d90c64c2ae0a2
pavel956 <slaysibir@yandex.ru>
2026-02-16 08:28:58 +0300

    6_2

---
+++ test_6_2.py
@@ -0,0 +1,16 @@
+ip = input()
+numbers = ip.split(".")
+numbers = [int(i) for i in numbers]
+print(numbers)
+if 223 >= numbers[0] >= 1:
+    print("unicast")
+elif 239 >= numbers[0] >= 224:
+    print("multicast")
+elif (
+    numbers[0] == 255 and numbers[1] == 255 and numbers[2] == 255 and numbers[3] == 255
+):
+    print("local broadcast")
+elif numbers[0] == 0 and numbers[1] == 0 and numbers[2] == 0 and numbers[3] == 0:
+    print("unassigned")
+else:
+    print("unused")
